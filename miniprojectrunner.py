925d7ec3ea020568e6e0a4acf6c83942de4dabc3
armin <arminjafri138386@gmail.com>
2023-09-09 11:10:14 -0400

    new def step

--- miniprojectrunner.py
+++ miniprojectrunner.py
@@ -1,5 +1,15 @@
-class runner:
-    def __int__(self):
+class Runner:
+    step = 0
+    def __int__(self, name, family,speed):
         self.x = x
         self.y = y
-    def 
+    with open ("informationrunner.txt") as my_runner:
+        print(my_runner.read())
+    def step(k):
+        if (k > 0 and k < 1):
+            result = k+Runner(k-1)
+            print(result)
+        else:
+            result = 0
+        return result
+step(6)
